37744bc3b19d7b0fabbf9aca4c2d575fbb06c961
TalonDragon000 <88998252+TalonDragon000@users.noreply.github.com>
2026-05-15 15:43:07 -1000

    Updated _layout.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -193,7 +193,7 @@ const styles = StyleSheet.create({
     bottom: 0,
     left: 0,
     right: 0,
-    height: 110,
+    height: 100,
     alignItems: 'center',
     justifyContent: 'center',
     zIndex: 30,
